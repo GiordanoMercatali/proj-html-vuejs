a4f01ce297f3f5ffd115109def9e4986ada71e13
GiordanoMercatali <mercataligiordano.dev@gmail.com>
2023-12-04 15:11:52 +0100

    add: app component files init

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+@use "../style/general.scss";
+@import "@fortawesome/fontawesome-free/css/all.css" ;
+    
+</style>
